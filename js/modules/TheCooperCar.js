5ee015de8fed91dcd972056c350b34817a13ae0d
macbook <macbook@macbooks-MacBook-Pro.local>
2020-12-01 21:54:39 +0200

    adding component

--- js/modules/TheCooperCar.js
+++ js/modules/TheCooperCar.js
@@ -1,36 +1,37 @@
 export default{
-        name: "TheCooperCar",
-        props:["cooper"],
-        data: function(){
-            return {
-                myModel: this.cooper.model,
-                myPrice: this.cooper.price,
-                myDesk: this.cooper.description,
-                //program: "IDP" make your own data message
-                myFeat: this.cooper.features
-            }
+    name: "TheCooperCar",
+    props:["cooper"],
+    data: function(){
+        return {
+            myModel: this.cooper.model,
+            myPrice: this.cooper.price,
+            myDesk: this.cooper.description,
+            //program: "IDP" make your own data message
+            myFeat: this.cooper.features
+        }
+    },
+
+    template: 
+    `<li @click="logClicked">
+        <p> {{ cooper.model }} </p>
+        <img class="showCooper" :src="'images/' + cooper.img1" alt='cooper.model + " img"'>
+           <a href="" class="removeCooper">
+           
+            </a> 
+            
+    </li>`,
+
+        created: function(){
+            console.log(`created ${this.cooper.model} cars`);
         },
 
-        template: 
-        `<li @click="logClicked">
-            <p> {{ cooper.model }} </p>
-            <img class="showCooper" :src="'images/' + cooper.img1" alt='cooper.model + " img"'>
-               <a href="" class="removeCooper">
-               
-                </a> 
-                
-        </li>`,
-
-            created: function(){
-                console.log(`created ${this.cooper.model} cars`);
-            },
-
-            methods:{
-                logClicked(){
-                    console.log(`fired from inside ${this.cooper.model} the component!`);
-                }
-                
+        methods:{
+            logClicked(){
+                console.log(`fired from inside ${this.cooper.model} the component!`);
+                this.$emit("showmydata", this.cooper)
             }
             
-   
+        }
+        
+
 }
